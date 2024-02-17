d406d6fb976a7212406416585f735f5057368e36
mateoghidini1998 <mateghidini123@gmail.com>
2024-02-17 16:26:16 -0300

    Add login and getme routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { protect } = require('../middleware/auth');
+
 
 const {
     register,
-    login
+    login,
+    getMe,
 } = require('../controllers/auth');
+
 
 router.post('/register', register);
 router.post('/', login);
+router.get('/me', protect, getMe);
 
 module.exports = router;
