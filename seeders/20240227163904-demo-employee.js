81dbd55dff8aa3f71f90813b2c3348a899c328e3
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-09 16:36:00 -0300

    Add seeders with sequelize cli

---
+++ seeders/20240227163904-demo-employee.js
@@ -0,0 +1,38 @@
+const { faker } = require('@faker-js/faker/locale/en');
+const User = require('../models/User.model');
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    function generateDni(){
+      const dni = faker.string.numeric(8);
+      return dni;
+    }
+
+    const users = await User.findAll();
+    const maxUserId = users.length;
+
+    const dummyJSON = [];
+    for(var i =  0 ; i <  maxUserId ; i++){
+      dummyJSON.push({
+        name: faker.person.firstName(),
+        lastname: faker.person.lastName(),
+        address: faker.location.streetAddress(),
+        phone: faker.phone.number(),
+        birthdate: faker.date.past(),
+        country: faker.location.country(),
+        dni: generateDni(),
+        position: faker.person.jobTitle(),
+        salary: faker.string.numeric(4),
+        user_id: users[i].id
+      });
+    }
+    await queryInterface.bulkDelete('Employees', null, {});
+    await queryInterface.bulkInsert('Employees', dummyJSON, {});
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Employees', null, {});
+  }
+};
