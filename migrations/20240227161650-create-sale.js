009006590ef54fd91e5bc72d972fc91f134850bf
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-09 16:34:40 -0300

    Create migrations

---
+++ migrations/20240227161650-create-sale.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      employee_id: {
+          type: Sequelize.INTEGER,
+          references: {
+              model: 'employees',
+              key: 'id'
+          },
+          required: true,
+          allowNull: false,
+      },
+      customer_id: {
+          type: Sequelize.INTEGER,
+          references: {
+              model: 'customers',
+              key: 'id'
+          },
+          required: true,
+          allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Sales');
+  }
+};
