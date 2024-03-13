a9981b7a94782a5059a09cf60865c147de3601e5
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-13 19:49:48 -0300

    Make code more readable

--- models/associations.js
+++ models/associations.js
@@ -3,10 +3,29 @@ const Employee = require('./Employee.model');
 const Sale = require('./Sale.model');
 const Service = require('./Service.model');
 
-Sale.belongsToMany(Service, { through: 'Sale_Service', onDelete: 'CASCADE' });
-Service.belongsToMany(Sale, { through: 'Sale_Service', onDelete: 'CASCADE' });
-User.hasOne(Employee, { foreignKey: 'user_id', as: 'Employee', onDelete: 'CASCADE' });
-Employee.belongsTo(User, { foreignKey: 'user_id', as: 'User', onDelete: 'CASCADE' });
+Sale.belongsToMany(Service, { 
+    through: 'SalesServices', 
+    as:'services', 
+    onDelete: 'CASCADE' 
+});
+
+Service.belongsToMany(Sale, { 
+    through: 'SalesServices', 
+    as:'sales', 
+    onDelete: 'CASCADE' 
+});
+
+User.hasOne(Employee, { 
+    foreignKey: 'user_id', 
+    as: 'Employee', 
+    onDelete: 'CASCADE' 
+});
+
+Employee.belongsTo(User, { 
+    foreignKey: 'user_id', 
+    as: 'User', 
+    onDelete: 'CASCADE' 
+});
 
 
 module.exports = { User, Employee, Sale, Service };
