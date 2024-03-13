46259f775d497cdd165fa427f2433261f991e408
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-13 19:48:52 -0300

    Rename model and migration

---
+++ migrations/20240229023643-create-salesservices.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = {
+ up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('SalesServices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      sale_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'sales',
+          key: 'id'
+        },
+        primaryKey: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE' 
+      },
+      service_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'services',
+          key: 'id'
+        },
+        primaryKey: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE' 
+      }
+    });
+ },
+ down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('SalesServices');
+ }
+};
---
+++ models/SalesServices.model.js
@@ -0,0 +1,33 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const SalesServices = sequelize.define('SalesServices', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    sale_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+            model: 'sales', 
+            key: 'id'
+        },
+    },
+    service_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+            model: 'services',
+            key: 'id'
+        },
+    }
+}, {
+    tableName: 'SalesServices', 
+    timestamps: false, 
+});
+
+module.exports = SalesServices;
